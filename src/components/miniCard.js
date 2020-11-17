b868ecdedfa4b42e176425c26d7db2cab3a46ad5
plaetzaw <plaetzaw@gmail.com>
2020-11-16 18:23:04 -0600

    Expermineting with React Multi Carousel

--- src/components/miniCard.js
+++ src/components/miniCard.js
@@ -1,34 +1,42 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import { CardMedia } from '@material-ui/core'
+import { Button } from '@material-ui/core';
 
-const miniCard = (props) => {
+
+const MiniCard = (props) => {
     const {title, overview, poster_path, backdrop_path} = props
-  return (
+
+    const [isToggled, setIsToggled] = React.useState('true')
+
+    const toggle = React.useCallback(() => setIsToggled(!isToggled), [isToggled, setIsToggled])
+  
+    let buttonMarkup = isToggled ? (<Button
+      style={{backgroundColor: 'red'}}>PosterPath</Button>) : 
+      (<Button style={{backgroundColor: 'green'}}>BackdropPath</Button>)
+
+    let urlPath = isToggled ? poster_path : (backdrop_path)
+    return (
     <>
       <Card
-      width="300px"
-      height="300px"
+      width="400px"
       >
+      <b>{title}</b>
+
         <CardMedia
         component="img"
         alt={title}
-        height='100px'
-        width='100px'
-        src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+        height='200px'
+        // width='100px'
+        src={`https://image.tmdb.org/t/p/original/${urlPath}`}
         />
-          <b>{title}</b>
-          <i>{overview}</i>
-          <CardMedia
-        component="img"
-        alt={title}
-        height='100px'
-        width='100px'
-        src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-        />
+         <Button onClick={toggle}>  
+          {buttonMarkup}
+          </Button>
+          {/* <i>{overview}</i> */}
       </Card>
     </>
   )
 }
 
-export default miniCard
+export default MiniCard
--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { GetMoviesHome } from "../redux/actions/actions"
 import MiniCard from '../components/miniCard'
 import LinearProgress from '@material-ui/core/LinearProgress';
-import '../utility/layout.css'
-// import { Carousel } from 'react-responsive-carousel';
+// import '../utility/layout.css'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 class Movies extends Component {
 
-    async componentDidMount(){
+    componentDidMount(){
         this.props.GetMoviesHome();
     }
-
+    
     render() {
+        const responsive = {
+            desktop: {
+              breakpoint: { max: 3000, min: 1024 },
+              items: 3,
+              slidesToSlide: 3 // optional, default to 1.
+            },
+            tablet: {
+              breakpoint: { max: 1024, min: 464 },
+              items: 2,
+              slidesToSlide: 2 // optional, default to 1.
+            },
+            mobile: {
+              breakpoint: { max: 464, min: 0 },
+              items: 1,
+              slidesToSlide: 1 // optional, default to 1.
+            }
+          };
+
         console.log(this.props.data.dataLoaded)
         const { data } = this.props
         const hotData = data.popularMovies.results
@@ -27,10 +45,12 @@ class Movies extends Component {
                 overview={cards.overview}
                 poster_path={cards.poster_path}
                 backdrop_path={cards.backdrop_path}
+                draggable={true}
                 />
             })
            } else {
-               hotMap = <>
+               hotMap = 
+               <>
                <h1>Loading</h1>
                </>
            }
@@ -38,13 +58,13 @@ class Movies extends Component {
         const hotMarkup = !this.props.data.dataLoaded ? (<><LinearProgress/><h1>Loading</h1><LinearProgress/></>) : (<><h1>{hotMap}</h1></>)
 
         return (
-            <>
-            <div 
-            className="movieDisplay"
-            >
-                {hotMarkup}
-            </div> 
-            </> 
+<>
+<Carousel
+  responsive={responsive}
+>
+{hotMarkup}
+</Carousel>
+</> 
         )
     }
 }
