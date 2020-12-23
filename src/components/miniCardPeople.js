9bcdf8c59c4a2542c6f31971db7ed60c447cc537
plaetzaw <plaetzaw@gmail.com>
2020-12-22 23:08:58 -0600

    Finished for tonight

--- src/components/miniCardPeople.js
+++ src/components/miniCardPeople.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card'
 import { CardMedia } from '@material-ui/core'
 import { Button } from '@material-ui/core';
 import { GetPersonInfo } from '../redux/actions/actions'
-import { People } from './popularPeople';
 
 
 
